6b56f78ab7a3e6560690c791575867ecbc55bc80
Rafael Dalepiane <rafadalepi@gmail.com>
2024-05-11 15:32:34 -0300

    Initial Commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function switch_to_image(){
+    var videoPlayer = document.getElementById('video');
+    var imageSwitch = document.getElementById('imagem');
+  
+    videoPlayer.style.display = 'none';
+    imageSwitch.style.display = 'grid';
+}
+
